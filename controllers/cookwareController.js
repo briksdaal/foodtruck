29c14a31e76e8935f9666cf1f19a2663859cd883
briksdaal <102250834+briksdaal@users.noreply.github.com>
2024-01-11 19:05:50 +0200

    Create cookware delete view and controller

--- controllers/cookwareController.js
+++ controllers/cookwareController.js
@@ -1,5 +1,6 @@
 const Cookware = require('../models/cookware');
 const CookwareInstance = require('../models/cookwareinstance');
+const Recipe = require('../models/recipe');
 const asyncHandler = require('express-async-handler');
 const { body, validationResult } = require('express-validator');
 
@@ -96,12 +97,52 @@ exports.cookware_create_post = [
 
 // Dispaly Cookware delete form on GET
 exports.cookware_delete_get = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Cookware delete GET');
+  // Get details of cookware and all cookware instances and recipes using it
+  const [cookware, allInstancesOfCookware, allRecipesWithCookware] =
+    await Promise.all([
+      Cookware.findById(req.params.id).exec(),
+      CookwareInstance.find({ cookware: req.params.id }).exec(),
+      Recipe.find({ cookware: req.params.id }).exec(),
+    ]);
+
+  if (cookware === null) {
+    // No results.
+    res.redirect('/cookware');
+    return;
+  }
+
+  res.render('cookware_delete', {
+    title: `Delete Cookware Type - ${cookware.title}`,
+    cookware: cookware,
+    cookwareinstance_list: allInstancesOfCookware,
+    recipe_list: allRecipesWithCookware,
+  });
 });
 
 // Handle Cookware delete on POST
 exports.cookware_delete_post = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Cookware delete POST');
+  // Get details of cookware and all cookware instances and recipes using it
+  const [cookware, allInstancesOfCookware, allRecipesWithCookware] =
+    await Promise.all([
+      Cookware.findById(req.params.id).exec(),
+      CookwareInstance.find({ cookware: req.params.id }, 'title').exec(),
+      Recipe.find({ cookware: req.params.id }, 'title').exec(),
+    ]);
+
+  if (allInstancesOfCookware.length > 0 || allRecipesWithCookware.length > 0) {
+    // Cookware has instances or recipes using it. Render in the same way as for GET route
+    res.render('cookware_delete', {
+      title: `Delete Cookware Type - ${cookware.title}`,
+      cookware: cookware,
+      cookwareinstance_list: allInstancesOfCookware,
+      recipe_list: allRecipesWithCookware,
+    });
+    return;
+  } else {
+    // Cookware has no instances or recipes using it. Delete object and redirect to cookware list
+    await Cookware.findByIdAndDelete(req.body.cookwareid);
+    res.redirect('/cookware');
+  }
 });
 
 // Dispaly Cookware update form on GET
